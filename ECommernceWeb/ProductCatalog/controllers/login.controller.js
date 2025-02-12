49a3f4b4cfbc5a8c9349de6845a5b0f46187c20d
Chavan Lakhan <tschalak@ap.dtcs.local>
2025-02-12 11:33:07 +0530

    Product Catalog Assignment

---
+++ ECommernceWeb/ProductCatalog/controllers/login.controller.js
@@ -0,0 +1,29 @@
+app.controller("LoginController", function ($scope, $window) {
+    $scope.user = { email: "", password: "" };
+
+    $scope.users = [
+        { email: "lakhanchavan1106@gmail.com", password: "test" },
+        { email: "abc@gmail.com", password: "234" },
+        { email: "lakhan@gmail.com", password: "lakhan" }
+    ];
+
+    $scope.onValidate = function (event) {
+        event.preventDefault(); 
+
+        var matchedUser = $scope.users.find(user =>
+            user.email === $scope.user.email && user.password === $scope.user.password
+        );
+
+        if (matchedUser) {
+            localStorage.setItem("loggedInUser", matchedUser.email);
+            console.log("Login");
+            $scope.$applyAsync(function () {
+                $window.location.href = "index.html";
+            });
+        } else {
+            console.error("Invalid credentials");
+            $scope.errorMessage = "Invalid credentials";
+            $scope.$apply();
+        }
+    };
+});
---
+++ ECommernceWeb/ProductCatalog/controllers/products.controller.js
@@ -0,0 +1,12 @@
+app.controller("ProductController", function ($scope) {
+    $scope.products = [
+        { name: "Monitor", price: 15000, image: "monitor.JPG", description: "High definition Monitor" },
+        { name: "Keyboard", price: 2000, image: "keyboard.JPG", description: "Keyboard" },
+        { name: "Laptop", price: 50000, image: "laptop.jpg", description: "High performance Laptop" },
+        { name: "Mouse", price: 250, image: "mouse.jpg", description: "Mouse" },
+        { name: "Headphones", price: 1500, image: "headphones.jpg", description: "Noise cancelling Headphones" },
+        { name: "Smartphone", price: 75000, image: "smartphone.jpg", description: "Latest Iphone" },
+        { name: "Tablet", price: 80000, image: "tablet.jpg", description: "Android Tablet" },
+        { name: "Smartwatch", price: 3500, image: "smartwatch.jpg", description: "Fitness Tracker Smartwatch" }
+    ];
+});
